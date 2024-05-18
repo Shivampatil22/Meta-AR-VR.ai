de20e07de9b866a104cfcae4cf55567dfa826bba
Him7n <knockknockletthedevilin777@proton.me>
2024-05-18 18:05:29 +0530

    hosted  resztsztv and 7used extensions t oshow it on the iframe

--- src/Iframe/ThreatreScreen.js
+++ src/Iframe/ThreatreScreen.js
@@ -4,9 +4,9 @@ import React from 'react'
 const ThreatreScreen = () => {
     return (<>
 
-        <Html occlude="blending" zIndexRange={[10, 1]} transform position={[0, 10, -179.5]}
-            scale={1}  >
-            <iframe width={"1450px"} height={"700vh"} src="https://www.bugswriter.com/work/" frameborder="5" />
+        <Html occlude="blending" zIndexRange={[10, 1]} transform position={[0, 13, -179.5]}
+            scale={1.3}  >
+            <iframe width={"1450px"} height={"700vh"} src="chrome-extension://opmeopcambhfimffbomjgemehjkbbmji/pages/player.html#http://localhost:8409/iptv/channel/2.m3u8?mode=segmenter" frameborder="5" />
 
         </Html>
     </>
