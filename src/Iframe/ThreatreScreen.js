469ab4757a0010285e99552080ece6413dbacc84
Him7n <knockknockletthedevilin777@proton.me>
2024-05-18 22:34:35 +0530

    added Office setup

--- src/Iframe/ThreatreScreen.js
+++ src/Iframe/ThreatreScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ThreatreScreen = () => {
     return (<>
 
-        <Html occlude="blending" zIndexRange={[10, 1]} transform position={[0, 13, -179.5]}
+        <Html occlude="blending" zIndexRange={[10, 1]} transform position={[0, 1, -179.5]}
             scale={1.3}  >
             <iframe width={"1450px"} height={"700vh"} src="chrome-extension://opmeopcambhfimffbomjgemehjkbbmji/pages/player.html#http://localhost:8409/iptv/channel/2.m3u8?mode=segmenter" frameborder="5" />
 
--- src/Models/Office.js
+++ src/Models/Office.js
@@ -1,13 +1,19 @@
-import { useGLTF } from '@react-three/drei'
+import { useFBX, useGLTF } from '@react-three/drei'
 import React, { Suspense } from 'react'
+import { MeshNormalMaterial, PlaneGeometry } from 'three';
 
 const Office = () => {
-    const OfficeModel = useGLTF("./models/mersus_office.glb");
+    const OfficeModel = useFBX("./models/OnlyTables2.fbx");
 
-
+    const room = useGLTF('./models/office_empty.glb');
 
     return (<Suspense>
-        <primitive scale={3} position={[-50, -1.1, 0]} object={OfficeModel.scene} />
+        <mesh castShadow position-x={6} scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <primitive scale={0.025} rotation-y={Math.PI / 2} position={[-76, -1, -11]} object={OfficeModel} />
+        <primitive scale={0.004} position={[-50, -1.1, 6]} object={room.scene} rotation-y={Math.PI / 2} />
     </Suspense>
     )
 }
