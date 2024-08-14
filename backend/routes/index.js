a057e0bb36d168cd7113d68657b821f885bc510c
Shivampatil22 <122476829+Shivampatil22@users.noreply.github.com>
2024-08-14 10:30:06 +0530

    Attendance model and route added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ import studentRouter from "./student.js"
 import teacherRouter from "./teacher.js"
 import assignmentRouter from "./assignment.js"
 import assignmentSubmissionRouter from "./assignment_submission.js"
-
+import attendanceRouter from "./attendance.js"
 const router = express.Router();
 
 router.use("/user", userRouter);
@@ -13,6 +13,7 @@ router.use("/account", accountRouter);
 router.use("/student",studentRouter);
 router.use("/teacher",teacherRouter);
 router.use("/assignment",assignmentRouter);
-router.use("/submission",assignmentSubmissionRouter)
+router.use("/submission",assignmentSubmissionRouter);
+router.use("/attendance", attendanceRouter);
 
 export default router;
