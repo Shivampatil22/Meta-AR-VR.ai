f8a37e5c98108d84239f44ef8783e06a7f222c5d
Shivampatil22 <122476829+Shivampatil22@users.noreply.github.com>
2024-08-16 01:29:49 +0530

    minor fix

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -2,6 +2,6 @@
 
 export function Button({label, onClick}) {
     return <button onClick={onClick} type="button" 
-    class="w-full text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 py-[20px] me2 mb-2">{label}</button>
+    class="w-full text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 py-[10px] me2 mb-2">{label}</button>
 }
   
